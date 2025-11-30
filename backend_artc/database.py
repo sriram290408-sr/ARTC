4d49717b135b338030606c70d3d6e8a0e4f15bb2
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-11-30 13:26:23 +0530

    delete some files

--- backend_artc/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DB_URL = "postgresql://postgres:AcademyRootPassword@localhost:5432/Artc"
-
-engine = create_engine(DB_URL)
-Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-Base = declarative_base()
