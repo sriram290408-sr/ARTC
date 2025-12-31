c7f9a7bcf297d7d44fbff1a255aa29815e189c98
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-12-31 19:42:43 +0530

    removal2

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
-from routers import user, report, committee, action_taken, login
+from routers import user, report, committee,  login
 
 app = FastAPI(title="ARTC Backend - Final")
 
@@ -20,7 +20,6 @@ Base.metadata.create_all(bind=engine)
 app.include_router(user.router)
 app.include_router(report.router)
 app.include_router(committee.router)
-app.include_router(action_taken.router)
 app.include_router(login.router)
 
 # Root endpoint
