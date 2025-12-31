a410ae80ce4d24e068d4eb07a8e4e310794c3055
sriramesakkimuthu-art <sriram290408@gmail.com>
2025-12-31 20:14:44 +0530

    dburl fixed

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE_URL = os.getenv("DB_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DB_URL environment variable is not set")
+    raise ValueError("DATABASE_URL environment variable is not set")
