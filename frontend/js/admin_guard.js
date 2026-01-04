ac364450496aea66f92a421023526436efe1f665
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-01-04 20:22:09 +0530

    fix guard.js

--- frontend/js/admin_guard.js
+++ frontend/js/admin_guard.js
@@ -1,6 +1,6 @@
+const token = localStorage.getItem("access_token");
 const role = localStorage.getItem("role");
-const token = localStorage.getItem("token");
 
-if (!token || role !== "admin") {
-  window.location.href = "/admin.html";
+if (!token || (role !== "admin" && role !== "faculty")) {
+  window.location.href = "/index.html";
 }
--- frontend/js/user_guard.js
+++ frontend/js/user_guard.js
@@ -1,4 +1,4 @@
-const token = localStorage.getItem("token");
+const token = localStorage.getItem("access_token"); 
 const role = localStorage.getItem("role");
 
 if (!token || role !== "student") {
