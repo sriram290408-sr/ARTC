dbad1a0875f278f37e7551073a26d16b42158880
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-01-04 19:22:33 +0530

    api url fix

--- frontend/js/report.js
+++ frontend/js/report.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", async (e) => {
   };
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/reports/create", {
+    const res = await fetch("https://artc-backend.onrender.com/reports/create", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
