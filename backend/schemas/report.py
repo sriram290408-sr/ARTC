cb8d5cc0003e2c7008d6f29630e7ff26d51bb0db
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-01-06 15:54:29 +0530

    fix in report

--- backend/schemas/report.py
+++ backend/schemas/report.py
@@ -1,16 +1,29 @@
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, date
+from typing import Optional
 
 class ReportBase(BaseModel):
     title: str
     description: str
+    problem_type: str
+    incident_location: str
+    incident_date: date
+    name: str
+    class_section: str
+    people_involved: Optional[str] = None
 
 class ReportCreate(ReportBase):
     pass
 
+class ReportUpdate(BaseModel):
+    status: str  
+    remarks: Optional[str] = None
+
 class ReportOut(ReportBase):
     id: int
+    status: str
     created_at: datetime
-    created_by: str
+    created_by: int
+
     class Config:
         from_attributes = True
