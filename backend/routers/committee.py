c002e1ed8aaf45948b4909a96d749def50edd412
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-01-07 11:09:50 +0530

    small spell error

--- backend/routers/committee.py
+++ backend/routers/committee.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from models.committee import CommitteeCreatea
+from models.committee import CommitteeCreate
 from schemas.committee import CommitteeCreate, CommitteeOut
 from dependencies.auth import get_current_user
 
