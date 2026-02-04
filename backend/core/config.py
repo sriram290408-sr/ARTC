3ed58d2ffc451c493e24e4e68bd0dd0e05208a0c
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-02-04 19:21:26 +0530

    small fix in backend

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,10 +1,12 @@
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY", "change_this_secret")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
-)
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
+
+SECRET_KEY = os.getenv("SECRET_KEY", "change_this_secret")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+)
