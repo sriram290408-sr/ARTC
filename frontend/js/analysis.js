3c21905fd9772871fb696ee88010d3943e961b30
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-02-19 09:56:55 +0530

    change server path

--- frontend/js/analysis.js
+++ frontend/js/analysis.js
@@ -1,7 +1,7 @@
 // for local host
-const API = "http://127.0.0.1:8000"; 
+// const API = "http://127.0.0.1:8000"; 
 // for render
-// const API = "https://artc-backend.onrender.com";
+const API = "https://artc-backend.onrender.com";
 
 
 let chart;
