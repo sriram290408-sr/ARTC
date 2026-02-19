606ee8a4c26fa035f70742093a6a02c6a11e8a33
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-02-19 15:10:18 +0530

    vercel connect

--- frontend/js/admin_analysis.js
+++ frontend/js/admin_analysis.js
@@ -1,8 +1,9 @@
 // for local host
 // const API = "http://127.0.0.1:8000"; 
 // for render
-const API = "https://artc-backend.onrender.com";
-
+// const API = "https://artc-backend.onrender.com";
+// for vercel
+const API = "https://artc-seven.vercel.app";
 
 let chart;
 
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,8 @@
-const API = "https://artc-backend.onrender.com";
+// for local host
 // const API = "http://127.0.0.1:8000";
+// for render
+// const API = "https://artc-backend.onrender.com";
+// for vercel
+const API = "https://artc-seven.vercel.app";
 
 export default API;
